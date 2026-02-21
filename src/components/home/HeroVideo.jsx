5dc22a4db607f2a14682391078c7613723fbe89c
Jerushaligne <jerushclinic@gmail.com>
2026-02-21 10:32:07 +0530

    new video again added

--- src/components/home/HeroVideo.jsx
+++ src/components/home/HeroVideo.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import "../../styles/herovideo.css";
 
 export default function BannerVideo({
-  videoSrc = "https://www.w3schools.com/html/mov_bbb.mp4",
+  videoSrc = "/videos/mov_bbb.mp4",
 }) {
   const videoRef = useRef(null);
 
