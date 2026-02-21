8fbfdc8bba2e06f1ed960a0fdd8e1179e763133a
Jerushaligne <jerushclinic@gmail.com>
2026-02-21 09:27:01 +0530

    added new video inside

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,40 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import { useEffect } from "react";
 
 import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import AboutPage from "./pages/AboutPage";
 import WhyJerushaligneIsDifferent from "./pages/WhyJerushaligneIsDifferent";
-import GalleryEvents from './pages/GalleryEvents'
-import ClearAligners from "./pages/ClearAligners"
+import GalleryEvents from "./pages/GalleryEvents";
+import ClearAligners from "./pages/ClearAligners";
 import Retainers from "./pages/Retainers";
 import OurOutlets from "./pages/OurOutlets";
 import ThuckalayOutlet from "./pages/outlets/ThuckalayOutlet";
 import TrichyOutlet from "./pages/outlets/TrichyOutlet";
-import ChennaiOutlet  from "./pages/outlets/ChennaiOutlet";
+import ChennaiOutlet from "./pages/outlets/ChennaiOutlet";
 import BlogPage from "./pages/BlogPage";
 import BlogListPage from "./pages/BlogListPage";
 import ContactUs from "./pages/ContactUs";
-import AppointmentSlots from "./pages/AppointmentSlots";
-import AdminSlots from "./pages/AdminSlots";
 
 import PageTransition from "./animations/PageTransition";
 import BackToTopIcon from "./components/BacktoTopIcon";
 import WhatsAppButton from "./components/WhatsAppButton";
 import FloatingSocialIcons from "./components/FloatingSocialIcons";
 
-
-
-
 function App() {
   const location = useLocation();
+
+  // ✅ Scroll To Top Fix Added Here
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   return (
     <>
       <NavBar />
 
-      {/* Page transition wrapper */}
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route
@@ -64,7 +64,6 @@ function App() {
             }
           />
 
-
           <Route
             path="/gallery-events"
             element={
@@ -83,7 +82,6 @@ function App() {
             }
           />
 
-
           <Route
             path="/retainers"
             element={
@@ -92,7 +90,6 @@ function App() {
               </PageTransition>
             }
           />
-
 
           <Route
             path="/why-jerushaligne-is-different"
@@ -103,7 +100,6 @@ function App() {
             }
           />
 
-
           <Route
             path="/our-outlets"
             element={
@@ -112,7 +108,6 @@ function App() {
               </PageTransition>
             }
           />
-
 
           <Route
             path="/outlets/thuckalay-outlet"
@@ -132,7 +127,7 @@ function App() {
             }
           />
 
-           <Route
+          <Route
             path="/outlets/chennai-outlet"
             element={
               <PageTransition>
@@ -141,7 +136,7 @@ function App() {
             }
           />
 
-         <Route
+          <Route
             path="/blog"
             element={
               <PageTransition>
@@ -150,7 +145,7 @@ function App() {
             }
           />
 
-           <Route
+          <Route
             path="/bloglist"
             element={
               <PageTransition>
@@ -158,7 +153,6 @@ function App() {
               </PageTransition>
             }
           />
-          
 
           <Route
             path="/contact-us"
@@ -168,21 +162,17 @@ function App() {
               </PageTransition>
             }
           />
-
         </Routes>
       </AnimatePresence>
-      
+
       <BackToTopIcon />
 
-      {/* Floating helpers */}
       <WhatsAppButton
         phone="+919876543210"
         message="Hello! I want to book an appointment."
       />
 
       <FloatingSocialIcons />
-
-
 
       <Footer />
     </>
